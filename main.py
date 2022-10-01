c43d28724ca1601bfa9b4e5957458b9fb4d9e938
30Dawid <114558712+XxDREFxX@users.noreply.github.com>
2022-10-01 13:57:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# zad 1
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+
+print(a**2 + b**2)
+# zad 2
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+
+print((a+b)**2)
+#zad 3
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+print((a-b)**3)
+#zad 4
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+c = int(input("Liczba C"))
+
+print(a*b*c)
+#zad 5
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+
+print(((a+b)*2)/5)
+#zad 6
+a = int(input("Suma Brutto:"))
+print(a / 1,23)
+#zad 7
+a = int(input("liczba A:"))
+b = int(input("liczba B:"))
+print(a%b)
